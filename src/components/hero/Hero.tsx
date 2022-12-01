26042c7b5b4002bd5c5c50a9d4943f474f5c3713
Andrew <an.carev4@gmail.com>
2022-12-01 13:09:04 +0300

    refactor; update `Hero` information

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import './hero.css';
-import HERO_IMG_LINK from '../../assets/images/hero.jpg';
-import CV_PDF_LINK from '../../assets/files/CV_Andrew_Karev_Front-end_Developer.pdf';
+import React from "react";
+import "./hero.css";
+import HERO_IMG_LINK from "../../assets/images/hero.jpg";
+import CV_PDF_LINK from "../../assets/files/CV_Andrew_Karev_Front-end_Developer.pdf";
 
 const Hero = () => {
   return (
@@ -9,13 +9,14 @@ const Hero = () => {
       <section className="hero-section">
         <h1 className="app-title">Welcome</h1>
         <p className="app-description">
-          My name is Andrew and I'm Front-end Developer. I'm familiar with Agile. I have knowledge of tools for creating web applications. I have experience in creating applications applying best coding practice like DRY, KISS and YAGNI. I'm interested in gaining knowledge and applying them to the real projects. At the moment i'm learning React and looking forward to working in teams as well as individually.
+          My name is Andrew and I'm Front-end Developer. I'm familiar with
+          Agile. I have knowledge of tools for creating web applications. I have
+          experience in creating applications applying best coding practice like
+          DRY, KISS and YAGNI. I'm interested in gaining knowledge and applying
+          them to the real projects. At the moment i'm learning Node.js and
+          looking forward to working in teams as well as individually.
         </p>
-        <a
-          className="learn-more-btn"
-          href={CV_PDF_LINK}
-          download={true}
-        >
+        <a className="learn-more-btn" href={CV_PDF_LINK} download={true}>
           <p className="learn-more-btn-content">Download CV</p>
         </a>
       </section>
