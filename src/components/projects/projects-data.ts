864dc0b5322adf3665c5093543ea5b323caca564
Andrew <an.carev4@gmail.com>
2022-12-01 12:08:41 +0300

    refactor: change projects images extension

--- src/components/projects/projects-data.ts
+++ src/components/projects/projects-data.ts
@@ -1,84 +1,88 @@
-import EASYLANG_IMG from '../../assets/images/projects/easyLang.png';
-import ONLINE_STORE_IMG from '../../assets/images/projects/online-store.png';
-import SHELTER_IMG from '../../assets/images/projects/shelter.png';
-import KEYBOARD_IMG from '../../assets/images/projects/virtual-keyboard.png';
-import MEMORY_GAME_IMG from '../../assets/images/projects/memory-game.png';
-import CSS_SLIDER_IMG from '../../assets/images/projects/CSS-slider.png';
-import PORTFOLIO_IMG from '../../assets/images/projects/portfolio.png';
-import VIDEO_PLAYER_IMG from '../../assets/images/projects/HTML5-video-player.png';
-import TIC_TAC_TOE_IMG from '../../assets/images/projects/tic-tac-toe.png';
+import EASYLANG_IMG from "../../assets/images/projects/easyLang.webp";
+import ONLINE_STORE_IMG from "../../assets/images/projects/online-store.webp";
+import SHELTER_IMG from "../../assets/images/projects/shelter.webp";
+import KEYBOARD_IMG from "../../assets/images/projects/virtual-keyboard.webp";
+import MEMORY_GAME_IMG from "../../assets/images/projects/memory-game.webp";
+import CSS_SLIDER_IMG from "../../assets/images/projects/CSS-slider.webp";
+import PORTFOLIO_IMG from "../../assets/images/projects/portfolio.webp";
+import VIDEO_PLAYER_IMG from "../../assets/images/projects/HTML5-video-player.webp";
+import TIC_TAC_TOE_IMG from "../../assets/images/projects/tic-tac-toe.webp";
 
 const projectsData = [
   {
-    title: 'EasyLang',
-    description: 'App for learning 3500+ English words with games and built-in textbook',
-    technologies: ['React', 'Typescript'],
-    code: 'https://github.com/andrewkarev/rslang',
-    deploy: 'https://rslang-andrewkarev.netlify.app/',
+    title: "EasyLang",
+    description:
+      "App for learning 3500+ English words with games and built-in textbook",
+    technologies: ["React", "Typescript"],
+    code: "https://github.com/andrewkarev/rslang",
+    deploy: "https://rslang-andrewkarev.netlify.app/",
     img: EASYLANG_IMG,
   },
   {
-    title: 'Online store',
-    description: 'An example of E-Commerce-Shop',
-    technologies: ['TypeScript', 'Sass'],
-    code: 'https://github.com/andrewkarev/RSS-FE-tasks/tree/online-store',
-    deploy: 'https://andrewkarev.github.io/RSS-FE-tasks/online-store/',
+    title: "Online store",
+    description: "An example of E-Commerce-Shop",
+    technologies: ["TypeScript", "Sass"],
+    code: "https://github.com/andrewkarev/RSS-FE-tasks/tree/online-store",
+    deploy: "https://andrewkarev.github.io/RSS-FE-tasks/online-store/",
     img: ONLINE_STORE_IMG,
   },
   {
-    title: 'Shelter',
-    description: 'An example of a charity site adapted to small devices',
-    technologies: ['JavaScript', 'HTML', 'Sass'],
-    code: 'https://github.com/andrewkarev/RSS-FE-tasks/tree/shelter',
-    deploy: 'https://andrewkarev.github.io/RSS-FE-tasks/shelter/pages/main/index.html',
+    title: "Shelter",
+    description: "An example of a charity site adapted to small devices",
+    technologies: ["JavaScript", "HTML", "Sass"],
+    code: "https://github.com/andrewkarev/RSS-FE-tasks/tree/shelter",
+    deploy:
+      "https://andrewkarev.github.io/RSS-FE-tasks/shelter/pages/main/index.html",
     img: SHELTER_IMG,
   },
   {
-    title: 'Virtual Keyboard',
-    description: 'An application that is a keyboard and allows you to enter text both with a mouse and with a real keyboard',
-    technologies: ['JavaScript', 'Sass'],
-    code: 'https://github.com/andrewkarev/virtual-keyboard/tree/main',
-    deploy: 'https://andrewkarev.github.io/virtual-keyboard/virtual-keyboard/',
+    title: "Virtual Keyboard",
+    description:
+      "An application that is a keyboard and allows you to enter text both with a mouse and with a real keyboard",
+    technologies: ["JavaScript", "Sass"],
+    code: "https://github.com/andrewkarev/virtual-keyboard/tree/main",
+    deploy: "https://andrewkarev.github.io/virtual-keyboard/virtual-keyboard/",
     img: KEYBOARD_IMG,
   },
   {
-    title: 'Memory game',
-    description: 'Card memory game with 2 game modes',
-    technologies: ['JavaScript', 'HTML', 'CSS'],
-    code: 'https://github.com/andrewkarev/RSS-Preschool-tasks/tree/memory-game',
-    deploy: 'https://andrewkarev.github.io/RSS-Preschool-tasks/memory-game/',
+    title: "Memory game",
+    description: "Card memory game with 2 game modes",
+    technologies: ["JavaScript", "HTML", "CSS"],
+    code: "https://github.com/andrewkarev/RSS-Preschool-tasks/tree/memory-game",
+    deploy: "https://andrewkarev.github.io/RSS-Preschool-tasks/memory-game/",
     img: MEMORY_GAME_IMG,
   },
   {
-    title: 'CSS slider',
-    description: 'Pure CSS responsive slider',
-    technologies: ['CSS', 'HTML'],
-    code: 'https://github.com/andrewkarev/cssMemSlider/tree/gh-pages',
-    deploy: 'https://andrewkarev.github.io/cssMemSlider/cssMemSlider/',
+    title: "CSS slider",
+    description: "Pure CSS responsive slider",
+    technologies: ["CSS", "HTML"],
+    code: "https://github.com/andrewkarev/cssMemSlider/tree/gh-pages",
+    deploy: "https://andrewkarev.github.io/cssMemSlider/cssMemSlider/",
     img: CSS_SLIDER_IMG,
   },
   {
-    title: 'Portfolio',
-    description: 'An example of Portfolio site adapted for small devices',
-    technologies: ['JavaScript', 'HTML', 'CSS'],
-    code: 'https://github.com/andrewkarev/RSS-Preschool-tasks/tree/portfolio',
-    deploy: 'https://andrewkarev.github.io/RSS-Preschool-tasks/portfolio/',
+    title: "Portfolio",
+    description: "An example of Portfolio site adapted for small devices",
+    technologies: ["JavaScript", "HTML", "CSS"],
+    code: "https://github.com/andrewkarev/RSS-Preschool-tasks/tree/portfolio",
+    deploy: "https://andrewkarev.github.io/RSS-Preschool-tasks/portfolio/",
     img: PORTFOLIO_IMG,
   },
   {
-    title: 'HTML5 Video Player',
-    description: 'Custom HTML5 Video Player',
-    technologies: ['JavaScript', 'HTML', 'CSS'],
-    code: 'https://github.com/andrewkarev/RSS-Preschool-tasks/tree/video-player',
-    deploy: 'https://andrewkarev.github.io/RSS-Preschool-tasks/portfolio/#video-section',
+    title: "HTML5 Video Player",
+    description: "Custom HTML5 Video Player",
+    technologies: ["JavaScript", "HTML", "CSS"],
+    code: "https://github.com/andrewkarev/RSS-Preschool-tasks/tree/video-player",
+    deploy:
+      "https://andrewkarev.github.io/RSS-Preschool-tasks/portfolio/#video-section",
     img: VIDEO_PLAYER_IMG,
   },
   {
-    title: 'Tic Tac Toe',
-    description: 'Tic Tac Toe game with win counter',
-    technologies: ['JavaScript', 'HTML', 'CSS'],
-    code: 'https://github.com/andrewkarev/tic-tac-toe',
-    deploy: 'https://andrewkarev.github.io/tic-tac-toe/',
+    title: "Tic Tac Toe",
+    description: "Tic Tac Toe game with win counter",
+    technologies: ["JavaScript", "HTML", "CSS"],
+    code: "https://github.com/andrewkarev/tic-tac-toe",
+    deploy: "https://andrewkarev.github.io/tic-tac-toe/",
     img: TIC_TAC_TOE_IMG,
   },
 ];
