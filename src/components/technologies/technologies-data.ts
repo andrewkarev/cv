737a501e2487eaa52a4d46a617ed579b16337a74
Andrew <an.carev4@gmail.com>
2022-12-01 12:25:14 +0300

    feat: add new technologies

--- src/components/technologies/technologies-data.ts
+++ src/components/technologies/technologies-data.ts
@@ -1,83 +1,101 @@
-import HTML_ICON from '../../assets/icons/html.png';
-import CSS_ICON from '../../assets/icons/css.png';
-import JAVASCRIPT_ICON from '../../assets/icons/javascript.png';
-import TYPESCRIPT_ICON from '../../assets/icons/typescript.png';
-import BEM_ICON from '../../assets/icons/bem.png';
-import SASS_ICON from '../../assets/icons/sass.png';
-import FIGMA_ICON from '../../assets/icons/figma.png';
-import GIT_ICON from '../../assets/icons/git.png';
-import JEST_ICON from '../../assets/icons/jest.png';
-import REST_API_ICON from '../../assets/icons/rest-api.png';
-import WEBPACK_ICON from '../../assets/icons/webpack.png';
-import ESLINT_ICON from '../../assets/icons/eslint.png';
-import BABEL_ICON from '../../assets/icons/babel.png';
+import HTML_ICON from "../../assets/icons/html.png";
+import CSS_ICON from "../../assets/icons/css.png";
+import JAVASCRIPT_ICON from "../../assets/icons/javascript.png";
+import TYPESCRIPT_ICON from "../../assets/icons/typescript.png";
+import BEM_ICON from "../../assets/icons/bem.png";
+import SASS_ICON from "../../assets/icons/sass.png";
+import FIGMA_ICON from "../../assets/icons/figma.png";
+import GIT_ICON from "../../assets/icons/git.png";
+import JEST_ICON from "../../assets/icons/jest.png";
+import REST_API_ICON from "../../assets/icons/rest-api.png";
+import WEBPACK_ICON from "../../assets/icons/webpack.png";
+import ESLINT_ICON from "../../assets/icons/eslint.png";
+import BABEL_ICON from "../../assets/icons/babel.png";
+import REACT_ICON from "../../assets/icons/react.png";
+import RTK_ICON from "../../assets/icons/redux.png";
+import REACT_HOOK_FORM_ICON from "../../assets/icons/react-hook-form.png";
 
 const technologies = [
   {
-    title: 'HTML5',
+    title: "HTML5",
     icon: HTML_ICON,
-    url: 'https://www.w3schools.com/html/default.asp',
+    url: "https://www.w3schools.com/html/default.asp",
   },
   {
-    title: 'CSS3',
+    title: "CSS3",
     icon: CSS_ICON,
-    url: 'https://www.w3schools.com/css/default.asp',
+    url: "https://www.w3schools.com/css/default.asp",
   },
   {
-    title: 'JavaScript',
+    title: "JavaScript",
     icon: JAVASCRIPT_ICON,
-    url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
   },
   {
-    title: 'TypeScript',
+    title: "TypeScript",
     icon: TYPESCRIPT_ICON,
-    url: 'https://www.typescriptlang.org/',
+    url: "https://www.typescriptlang.org/",
   },
   {
-    title: 'Sass',
+    title: "React",
+    icon: REACT_ICON,
+    url: "https://beta.reactjs.org/",
+  },
+  {
+    title: "Redux Toolkit",
+    icon: RTK_ICON,
+    url: "https://redux-toolkit.js.org/",
+  },
+  {
+    title: "React Hook Form",
+    icon: REACT_HOOK_FORM_ICON,
+    url: "https://react-hook-form.com/",
+  },
+  {
+    title: "Sass",
     icon: SASS_ICON,
-    url: 'https://sass-lang.com/',
+    url: "https://sass-lang.com/",
   },
   {
-    title: 'Figma',
+    title: "Figma",
     icon: FIGMA_ICON,
-    url: 'https://www.figma.com/',
+    url: "https://www.figma.com/",
   },
   {
-    title: 'Git',
+    title: "Git",
     icon: GIT_ICON,
-    url: 'https://git-scm.com/',
+    url: "https://git-scm.com/",
   },
   {
-    title: 'Jest',
+    title: "Jest",
     icon: JEST_ICON,
-    url: 'https://jestjs.io/',
+    url: "https://jestjs.io/",
   },
   {
-    title: 'REST API',
+    title: "REST API",
     icon: REST_API_ICON,
-    url: 'https://restfulapi.net/rest/',
+    url: "https://restfulapi.net/rest/",
   },
   {
-    title: 'Webpack',
+    title: "Webpack",
     icon: WEBPACK_ICON,
-    url: 'https://webpack.js.org/',
+    url: "https://webpack.js.org/",
   },
   {
-    title: 'ESLint',
+    title: "ESLint",
     icon: ESLINT_ICON,
-    url: 'https://eslint.org/',
+    url: "https://eslint.org/",
   },
   {
-    title: 'Babel',
+    title: "Babel",
     icon: BABEL_ICON,
-    url: 'https://babeljs.io/',
+    url: "https://babeljs.io/",
   },
   {
-    title: 'BEM',
+    title: "BEM",
     icon: BEM_ICON,
-    url: 'https://en.bem.info/methodology/',
+    url: "https://en.bem.info/methodology/",
   },
-]
+];
 
 export default technologies;
